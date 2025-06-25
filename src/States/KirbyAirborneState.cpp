68062510996f47cfd3c30c74eff84627693e8ee4
Keren (Cammy) Zaig <kerenza@edu.jmc.ac.il>
2025-06-25 09:56:01 +0300

    add multiple jumping

--- src/States/KirbyAirborneState.cpp
+++ src/States/KirbyAirborneState.cpp
@@ -1,11 +1,19 @@
 #include "States/KirbyAirborneState.h"
 #include "States/KirbyStandingState.h" // To transition to when we land
+#include "States/KirbyJumpingState.h" // To transition to when we jump
 #include "GameObj/MovingObj/Kirby.h"
 #include <SFML/Window/Keyboard.hpp>
 #include "GlobalSizes.h";
 
 std::unique_ptr<KirbyState> KirbyAirborneState::handleInput(Kirby& kirby)
 {
+	// **IMPROVEMENT**: Allow jumping even when in mid-air.
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		// This will allow you to jump out of a falling state.
+		return std::make_unique<KirbyJumpingState>();
+	}
+
 	// If the collision check has set our state to grounded,
 	// it's time to transition back to the standing state.
 	if (kirby.isGrounded())
