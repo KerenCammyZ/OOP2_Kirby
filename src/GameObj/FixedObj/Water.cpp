19e2acf7e4a73bd57a42130b645664451de2204b
Keren (Cammy) Zaig <kerenza@edu.jmc.ac.il>
2025-07-01 16:45:56 +0300

    add Level3 files

--- src/GameObj/FixedObj/Water.cpp
+++ src/GameObj/FixedObj/Water.cpp
@@ -1,26 +1,3 @@
-//#include "GameObj/FixedObj/Water.h"
-//#include "GameObj/MovingObj/Kirby.h"
-//#include "GameObjectFactory.h"
-//#include <SFML/Graphics.hpp>
-//
-//const sf::Color waterColor(0, 0, 200);
-//
-//// Register the new Water object with the factory
-//static bool isWaterRegistered = GameObjectFactory::registerType(
-//	waterColor,
-//	[](sf::Vector2f position) -> std::unique_ptr<GameObject>
-//	{
-//		return std::make_unique<Water>();
-//	}
-//);
-//
-//void Water::handleCollision(Kirby* kirby)
-//{
-//	// This is the core logic. When Kirby is inside a Water object,
-//	// we set his internal "in water" flag to true.
-//	kirby->setInWater(true);
-//}
-
 #include "GameObj/FixedObj/Water.h"
 #include "GameObj/MovingObj/Kirby.h"
 #include "GameObjectFactory.h"
