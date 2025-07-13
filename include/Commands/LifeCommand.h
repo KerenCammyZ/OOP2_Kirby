2df9d34762bfcd5999618edd92797d50a1c89b88
Keren (Cammy) Zaig <kerenza@edu.jmc.ac.il>
2025-07-13 10:59:47 +0300

    add life powerup

---
+++ include/Commands/LifeCommand.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Commands/PowerUpCommand.h"
+#include "GameObj/MovingObj/Kirby.h"
+
+class LifeCommand : public PowerUpCommand
+{
+public:
+	void execute(Kirby& kirby) override
+	{
+		// Tell Kirby to add one life.
+		kirby.addLife(1);
+	}
+
+	// Since this is an instant effect, undo does nothing.
+	void undo(Kirby& kirby) override {}
+
+	// A zero duration makes this an instant-use item.
+	sf::Time getDuration() const override
+	{
+		return sf::Time::Zero;
+	}
+};
