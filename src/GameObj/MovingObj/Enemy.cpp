fd2412ee08939b58bea9e542759887f366856b66
Keren (Cammy) Zaig <kerenza@edu.jmc.ac.il>
2025-07-14 23:05:07 +0300

    add help screen image

--- src/GameObj/MovingObj/Enemy.cpp
+++ src/GameObj/MovingObj/Enemy.cpp
@@ -13,7 +13,7 @@
 #include "Commands/SparkCommand.h"
 
 sf::Color WaddleDeeColor(255, 0, 110);
-sf::Color TwizzyColor(0, 0, 80);
+sf::Color TwizzyColor(255, 105, 180);
 sf::Color sparkyColor(178, 0, 255);
 
 // Static registration for WaddleDee
